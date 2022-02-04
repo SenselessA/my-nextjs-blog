b904e0dae6331af20896ff9be13ca9adb8e2a886
ChernyshovAA <nekrot3@gmail.com>
2022-02-04 15:54:45 +0300

    [feature] adding typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,11 +3,19 @@ import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import React from "react";
 
-const name = 'Chernyshov Andrew'
-export const siteTitle = `Chernyshov Andrew's blog`
+const name = 'Andrew Chernyshov'
+export const siteTitle = 'My next-js blog'
 
-export default function Layout({ children, home }) {
+export default function Layout(
+    {
+        children,
+        home
+    }: {
+    children: React.ReactNode
+    home?: boolean
+}) {
     return (
         <div className={styles.container}>
             <Head>
@@ -20,7 +28,7 @@ export default function Layout({ children, home }) {
                     property="og:image"
                     content={`https://og-image.vercel.app/${encodeURI(
                         siteTitle
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
--- lib/posts.tsx
+++ lib/posts.tsx
@@ -23,37 +23,21 @@ export function getSortedPostsData() {
         // Combine the data with the id
         return {
             id,
-            ...matterResult.data
-        }
+            ...(matterResult.data as { date: string; title: string })
+    }
     })
     // Sort posts by date
-    return allPostsData.sort(({ date: a }, { date: b }) => {
-        if (a < b) {
+    return allPostsData.sort((a, b) => {
+        if (a.date < b.date) {
             return 1
-        } else if (a > b) {
-            return -1
         } else {
-            return 0
+            return -1
         }
     })
 }
 
 export function getAllPostIds() {
     const fileNames = fs.readdirSync(postsDirectory)
-
-    // Returns an array that looks like this:
-    // [
-    //   {
-    //     params: {
-    //       id: 'ssg-ssr'
-    //     }
-    //   },
-    //   {
-    //     params: {
-    //       id: 'pre-rendering'
-    //     }
-    //   }
-    // ]
     return fileNames.map(fileName => {
         return {
             params: {
@@ -63,7 +47,7 @@ export function getAllPostIds() {
     })
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -72,14 +56,14 @@ export async function getPostData(id) {
 
     // Use remark to convert markdown into HTML string
     const processedContent = await remark()
-        .use(html)
-        .process(matterResult.content)
+      .use(html)
+      .process(matterResult.content)
     const contentHtml = processedContent.toString()
 
     // Combine the data with the id and contentHtml
     return {
         id,
         contentHtml,
-        ...matterResult.data
-    }
+        ...(matterResult.data as { date: string; title: string })
+}
 }
